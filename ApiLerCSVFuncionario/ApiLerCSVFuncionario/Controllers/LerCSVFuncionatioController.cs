af8d2d419961f8f6982b4af7af4e0926e3947bc4
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2025-06-21 17:09:52 -0300

    CONFIG - configuracoes finais

---
+++ ApiLerCSVFuncionario/ApiLerCSVFuncionario/Controllers/LerCSVFuncionatioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLerCSVFuncionario.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LerCSVFuncionatioController : ControllerBase
+    {
+        private readonly ILogger<LerCSVFuncionatioController> _logger;
+
+        [HttpGet("/on")]
+        public Object AplicationOn()
+        {
+            return Ok("Teste");
+        }
+    }
+}
